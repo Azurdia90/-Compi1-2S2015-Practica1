ed6e84c65c7ce2cb28601350b54a14692c928b78
Cristian Azurdia <201020331@ingenieria.usac.edu.gt>
2015-08-08 09:39:40 -0600

    Día 1
    Creación del proyecto, definición de versión,  definición de dispositivo
    y de la interfaz grafica a utilizar.

---
+++ Practica1_201020331/app/src/androidTest/java/com/compi1/azurdia/practica1_201020331/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.compi1.azurdia.practica1_201020331;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
